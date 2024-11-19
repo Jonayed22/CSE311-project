b93ec298c856be830e93ac944bbc21f60ca3f46b
Jonayed Hossain <jonayedhossain235@gmail.com>
2024-11-19 22:03:57 +0600

    Add files via upload

---
+++ announcement.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+    <h1>School Announcements</h1>
+    <br>
+    <h4>Dear Students,
+        Your CSE311 project demonstration will be held on 1 December,2024</h4>
+        <a href="Spro.php">
+            <button>Home</button>
+        </a>
+        </center>
+</body>
+</html>
