f4fb4b39525016d2fac0cd8e0e0dcb73e0c899df
Jonayed Hossain <jonayedhossain235@gmail.com>
2024-11-19 22:05:24 +0600

    Add files via upload

---
+++ tea.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <br>
+        <h>
+            <h2>Our Teachers Names</h2>
+            <h4>Ishan Arefin</h4>
+            <h4>Dr.Rajesh Palit</h4>
+            <h4>Dr.Atiqur Rahman</h4>
+            <h4>Dr.Md Shahriar Karim</h4>
+            <h4>Dr.Shafin Rahman</h4>
+        </h>
+        <a href="Spro.php">
+            <button>Home</button>
+        </a>
+
+    </center>
+</body>
+</html>
