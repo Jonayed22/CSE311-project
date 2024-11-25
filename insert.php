83395c7cc57b23bdbf27c8c46b7bda5f65780cbe
Jonayed Hossain <jonayedhossain235@gmail.com>
2024-11-25 21:43:37 +0600

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <center>
+        <br><br><br><br><br><br><br>
+        <label>Where You Want To Insert</label><br><br>
+        <div>
+            <lebel><a href="department.php" class="btn btn-success">Department</a></lebel>
+            <lebel><a href="student.php" class="btn btn-success">Student</a></lebel>
+            <lebel><a href="takes.php" class="btn btn-success">Takes</a></lebel>
+            <lebel><a href="course.php" class="btn btn-success">Course</a></lebel>
+            <lebel><a href="section.php" class="btn btn-success">Section</a></lebel>
+            <lebel><a href="instructor.php" class="btn btn-success">Instructor</a></lebel>
+            <lebel><a href="parents.php" class="btn btn-success">Parents</a></lebel>
+            <lebel><a href="exam.php" class="btn btn-success">Exam</a></lebel>
+        </div>
+        <br>
+        <br>
+        <br>
+        <a href="login_prosses.php">
+            <button>Back</button>
+        </a>
+        <br><br>
+        <a href="Spro.php">
+            <button>Home</button>
+        </a>
+    </center>
+</body>
+
+</html>
